7afb8dcc97100a00226244d09f795c4b5e16de8a
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-05-01 23:18:02 +0500

    completed project 1 random color

--- bg-changer/index.js
+++ bg-changer/index.js
@@ -1,16 +1,46 @@
-let red = document.getElementById("red")
-let green = document.getElementById("green")
-let blue = document.getElementById("blue")
-let random = document.getElementById("blue")
+// This Is My Method ->
 
-green.addEventListener('click',()=>{
-    document.body.style.backgroundColor = "green"
-})
+// let red = document.getElementById("red")
+// let green = document.getElementById("green")
+// let blue = document.getElementById("blue")
+// let random = document.getElementById("blue")
 
-red.onclick = function (){
-    document.body.style.backgroundColor = "red"
+// let hex = 'abcdef0123456789'
+// let color = '#'
+
+// for (let i = 0; i < 5; i++) {
+//     color += Math.floor(Math.random() * 5 + 1)[i];
+//     console.log(color);
+    
+// }
+
+// green.addEventListener('click',()=>{
+//     document.body.style.backgroundColor = "green"
+// })
+
+// red.onclick = function (){
+//     document.body.style.backgroundColor = "red"
+// }
+// blue.onclick = function (){
+//     document.body.style.backgroundColor = "blue"
+// }
+
+
+// This Is Tim's Method ->
+
+const body = document.getElementsByTagName('body')[0]
+
+function setColor (name){
+    body.style.backgroundColor = name
 }
-blue.onclick = function (){
-    document.body.style.backgroundColor = "blue"
-}
 
+function randomColor(){
+    let red = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+
+    const color = `rgb(${red}, ${blue}, ${green})` 
+
+    body.style.backgroundColor = color
+}
+randomColor()
